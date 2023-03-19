64034f095f01a465ad58d9213686b2ca5df72cfe
Pedro Freitas <pedrospf2018@gmail.com>
2023-03-19 11:21:40 -0300

    Criação do projeto

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(8, 8);
+        System.out.println(board);
+
+    }
+}
